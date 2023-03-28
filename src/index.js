756f310741d764856c2dce41de065debb293a710
Tomas Gomila <TomasGomila_09@hotmail.com>
2023-03-28 11:36:13 -0300

    hola

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const app = require('express')()
+import express from "express";
 
-app.get('/src', (req, res)=>{
-    res.send(index.html)
+const app = express();
+const port = 3000;
+app.use("/", (req,res)=>{
+    res.json({mensage:"hola desde expreso aplicacioneses"})
 })
 
-module.exports = app
-
-
+app.listen(3000, () => {
+    console.log(`Server listo en el puerte ${port}`)
+})
